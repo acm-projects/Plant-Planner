8155234d9c8c79c875a7e59c73e540e243103dd6
saj62 <sarannjac@gmail.com>
2023-10-18 20:55:30 -0500

    pushing changes to github

--- app/src/main/java/com/example/plantplanner/SecondActivity.java
+++ app/src/main/java/com/example/plantplanner/SecondActivity.java
@@ -29,9 +29,9 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_page);
 
-        name = findViewById(R.id.editEmailText);
-        email = findViewById(R.id.editPasswordText);
-        signOutBtn = findViewById(R.id.google_btn);
+        name = findViewById(R.id.textView4);
+        email = findViewById(R.id.textView5);
+        signOutBtn = findViewById(R.id.button2);
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this,gso);
