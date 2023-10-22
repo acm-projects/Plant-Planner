8f5b98686a48e67f7699bcdd085e11401aaa37ac
uma776 <uma.uppuloori@gmail.com>
2023-10-21 21:57:11 -0500

    todo

--- app/src/main/java/com/example/plantplanner/CalendarActivity.java
+++ app/src/main/java/com/example/plantplanner/CalendarActivity.java
@@ -1,4 +1,11 @@
 package com.example.plantplanner;
 
 public class CalendarActivity {
+
+    //connect to search pg
+
+    //connect to current list pg
+
+    //connect to how to pg
+    //connect to profile
 }
--- app/src/main/java/com/example/plantplanner/CurrentPlantsActivity.java
+++ app/src/main/java/com/example/plantplanner/CurrentPlantsActivity.java
@@ -1,4 +1,11 @@
 package com.example.plantplanner;
 
 public class CurrentPlantsActivity {
+
+    //connect to search pg
+
+    //connect to calendar pg
+
+    //connect to how to pg
+    //connect to profile
 }
